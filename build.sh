0d82ab9a2c2af7f64d843a03ecacbf9584bd0cb0
junlinp <junlinp@deepmirror.com>
2024-12-16 11:09:05 +0800

    add files

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 current_path=$(pwd)
-cd nginx-1.26.2 && ./configure --add-module=${current_path}/ngx_http_proxy_connect_module && make && make install
+cd nginx-1.26.2 && ./configure --add-module=${current_path}/ngx_http_proxy_connect_module-0.0.7 && make && make install
